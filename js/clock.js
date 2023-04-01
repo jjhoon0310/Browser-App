1724b112853322d3cca09968c26c8196eea8dc05
jjhoon0310 <jjhoon0310@gmail.com>
2023-04-01 16:34:39 +0100

    Clock showing in US format

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,38 @@
 const clock = document.querySelector("h2#clock");
+const today = document.querySelector("h2#date");
 
 function getTime() {
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    var strTime = `${hours}:${minutes}:${seconds} ${ampm}`;
+    clock.innerText = strTime;
 }
 
+function getDate() {
+    const date = new Date();
+    const year = String(date.getFullYear());
+    const options = { month: "long", timeZone: "UTC" };
+    const monthName = date.toLocaleString("en-US", options);
+    let day = String(date.getDate());
+    if (day.at(-1) == 1) {
+        day = `${day}st`;
+    } else if (day.at(-1) == 2) {
+        day = `${day}nd`;
+    } else if (day.at(-1) == 3) {
+        day = `${day}rd`;
+    } else {
+        day = `${day}th`;
+    }
+    today.innerText = `${monthName}, ${day} ${year}`;
+}
+
+getDate();
 getTime();
 setInterval(getTime, 1000);
