174f24ab20454cc339f08d2a5bb4866fe85e29fe
jjhoon0310 <jjhoon0310@gmail.com>
2023-04-02 20:48:57 +0100

    Design (UI) update

--- js/background.js
+++ js/background.js
@@ -1,8 +1,14 @@
 const images = ["0.jpeg", "1.jpg"];
-
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+const link = document.querySelector("#link");
 
 const bgImage = document.createElement("img");
 bgImage.src = `images/${chosenImage}`;
+
+if (chosenImage == "0.jpeg") {
+    link.href = "styleDark.css";
+} else {
+    link.href = "style.css";
+}
 
 document.body.appendChild(bgImage);
